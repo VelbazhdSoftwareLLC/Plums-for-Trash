313325cfe40a7f90e4f2d2afeef107b8ee1b9434
Martinkata1 <marti.kostadinov2003@gmail.com>
2024-04-11 17:38:52 +0300

    Mapping of the images to keys.

--- app/src/main/java/eu/veldsoft/plums/trash/model/Card.java
+++ app/src/main/java/eu/veldsoft/plums/trash/model/Card.java
@@ -4,4 +4,26 @@ package eu.veldsoft.plums.trash.model;
  * Abstract class for cards.
  */
 abstract class Card {
+    /**
+     * Key value of the card used in some collections.
+     */
+    private String key = "";
+
+    /**
+     * Constructor with card key.
+     *
+     * @param key Key value used in some collections.
+     */
+    Card(String key) {
+        this.key = key;
+    }
+
+    /**
+     * Get card key.
+     *
+     * @return Card key.
+     */
+    String key() {
+        return key;
+    }
 }
--- app/src/main/java/eu/veldsoft/plums/trash/model/PlumsCard.java
+++ app/src/main/java/eu/veldsoft/plums/trash/model/PlumsCard.java
@@ -1,4 +1,12 @@
 package eu.veldsoft.plums.trash.model;
 
 class PlumsCard extends Card {
+    /**
+     * Constructor with card key.
+     *
+     * @param key Key value used in some collections.
+     */
+    PlumsCard(String key) {
+        super(key);
+    }
 }
