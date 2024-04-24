6a685509622cbd0de679d131451709a92fea3e5a
Martinkata1 <marti.kostadinov2003@gmail.com>
2024-04-24 22:13:05 +0300

    Context menu has been added.

--- app/src/main/java/eu/veldsoft/plums/trash/GameActivity.java
+++ app/src/main/java/eu/veldsoft/plums/trash/GameActivity.java
@@ -2,6 +2,8 @@ package eu.veldsoft.plums.trash;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -120,5 +122,16 @@ public class GameActivity extends Activity {
             CARDS_IMAGES.put("quest_v5_en_15", R.drawable.quest_v5_en_15);
             CARDS_IMAGES.put("quest_v5_en_16", R.drawable.quest_v5_en_16);
         }
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.game_option_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
